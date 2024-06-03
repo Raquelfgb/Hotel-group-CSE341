38265a912e8945a03e9219e25b5504809269ecea
Raquelfgb <kelkinha@hotmail.com>
2024-06-03 10:47:13 -0600

    auth

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'This is an API for HotelierPro application, providing endpoints for managing hotels, staff, clients, and bookings.',
     version: '1.0.0'
   },
-  host: 'hotelierpro-project.onrender.com', 
+  host: 'hotel-group-cse341.onrender.com', 
   schemes: ['https'], 
   securityDefinitions: {
     githubOAuth: {
